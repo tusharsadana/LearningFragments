0254901c39095df2e268fd63eb5460cf0ce5e36e
vidhyanshu <v.vidu171@gmail.com>
2018-07-17 01:30:35 +0530

    solved

--- app/src/main/java/com/acm/tusha/learningfragments/MainActivity.java
+++ app/src/main/java/com/acm/tusha/learningfragments/MainActivity.java
@@ -3,6 +3,7 @@ package com.acm.tusha.learningfragments;
 import android.app.FragmentManager;
 import android.app.Fragment;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void fragChange(View view){
         android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
         TestFragment acm = new TestFragment();
